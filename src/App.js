08e7778620ecaeaf21f2039f9991676c50e7a4d2
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2024-09-19 16:08:40 +0100

    Fixed issue in target"_blank"
    Added rel="nonreferrer"

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
           <a
             href="https://github.com/AnnaLSutherland/react-weather-app"
             target="_blank"
+            rel="noreferrer"
           >
             OpenSource code
           </a>
