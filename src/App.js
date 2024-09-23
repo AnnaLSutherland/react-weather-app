04dbb939588d4e5b6410105a65f236ff50fb9d60
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2024-09-23 15:23:12 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Anna Sutherland and is{" "}
-        <a
-          href="https://github.com/AnnaLSutherland/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced in GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>{" "}
+        <footer>
+          This project was coded by Anna Sutherland and is{" "}
+          <a
+            href="https://github.com/AnnaLSutherland/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced in GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
