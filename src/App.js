6339b0188b972d3bb9dc7a00c6c9ccdf33089ac9
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2024-09-23 15:00:05 +0100

    Added the GitHub link
    (and finished deleting un-needed code)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Anna Sutherland and is{" "}
+        <a
+          href="https://github.com/AnnaLSutherland/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced in GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
