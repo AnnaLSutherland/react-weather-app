b974c519782aed180b722e43a08c023543daf0fb
AnnaLSutherland <116560657+AnnaLSutherland@users.noreply.github.com>
2024-09-23 14:46:15 +0100

    Cleaned up files so I can create Weather App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
 import "./App.css";
-import Weather from "./Weather";
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather />
-        <small>
-          {" "}
-          <p>
-            <a
-              href="https://github.com/AnnaLSutherland/react-weather-app"
-              target="_blank"
-              rel="noreferrer"
-              className="githubLink"
-            >
-              OpenSource code
-            </a>
-            by Anna Sutherland
-          </p>
-        </small>
-      </header>
-    </div>
-  );
+  return <div className="App">Weather App</div>;
 }
